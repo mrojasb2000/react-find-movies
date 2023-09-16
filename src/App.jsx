c682a61c82eea2be4ed380ac59321d7b5135c86f
mavro <mrojasb2000@gmail.com>
2023-09-16 19:34:06 -0300

    chore: add basic form data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <h1>Welcome to React</h1>
+      <form className='form'>
+        <label>
+          Put the movie name
+          <input placeholder='Avenger, Star Wars, The Matrix...' />
+          <button type='submit'>Find</button>
+        </label>
+      </form>
     </>
   )
 }
